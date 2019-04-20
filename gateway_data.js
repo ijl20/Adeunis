7ce60a717f93758bfcd6275ff1950850e82672d0
Ian Lewis <ijl20@cam.ac.uk>
2019-04-20 08:08:54 +0100

    more data

---
+++ gateway_data.js
@@ -0,0 +1,99 @@
+gateway_data.push({ mac: '70B3D54B138E0000',
+                    lat: 52.21098,
+                    lng: 0.09185,
+                    alt: 37,
+                    name: 'CL',
+                    description: 'Cambridge William Gates Building (Computer Lab)'});
+
+
+/*
+gateway_data.push({ mac: '70B3D54B138E0000',
+                    lat: 52.21098,
+                    lng: 0.09185,
+                    alt: 37,
+                    name: 'CL',
+                    description: 'Cambridge William Gates Building (Computer Lab)'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-13-C8-00-00',
+                    lat: 52.20512,
+                    lng: 0.1084,
+                    alt: 70,
+                    name: 'UL',
+                    description: 'University Library'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52.1818,
+                    lng: 0.17851,
+                    alt: 20,
+                    name: 'Arm',
+                    description: 'Arm'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52.23259,
+                    lng: 0.1448,
+                    alt: 20,
+                    name: 'CC',
+                    description: 'Cambridge Consultants'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52.21176,
+                    lng: -0.01835,
+                    alt: 87,
+                    name: 'Phil',
+                    description: 'Phil Claridge'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52.20114,
+                    lng: 0.13053,
+                    alt: 36,
+                    name: 'KK',
+                    description: 'Kelsey Kerridge'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52.2061,
+                    lng: 0.09686,
+                    alt: 5,
+                    name: 'WR',
+                    description: 'Wilberforce Rd'});
+
+gateway_data.push({ mac: '70-B3-D5-4B-12-9D-00-00',
+                    lat: 52 + 11.69/60,
+                    lng: 0 + 8.11 / 60,
+                    alt: 5,
+                    name: 'MS',
+                    description: 'Microsoft'});
+*/
+/*
+2AE test gateway
+STATISTICS DEBUG
+    0-16-02-04-00-176
+Mantas test
+STATISTICS DEBUG
+    0-16-02-04-00-3C8
+University Library
+STATISTICS DEBUG
+    0-16-02-04-00-15C
+Sydney Sussex College
+STATISTICS DEBUG
+    0-16-02-04-00-14D
+Kelsey Kerridge
+STATISTICS DEBUG
+    0-16-02-04-00-14C
+Cambridge Consultants
+STATISTICS DEBUG
+    0-16-02-04-00-29D
+ARM
+STATISTICS DEBUG
+    0-16-02-04-00-1B4
+Microsoft
+STATISTICS DEBUG
+    0-16-02-04-00-2A5
+Shire Hall
+STATISTICS DEBUG
+    0-16-02-04-00-303
+Wilberforce Road Sports Ground
+STATISTICS DEBUG
+    0-16-02-04-00-249
+Phil Claridge
+STATISTICS DEBUG
+*/
---
+++ gateways.js
@@ -0,0 +1,41 @@
+gateway_data.push({ mac: '70B3D54B138E0000',
+                    lat: 52.21098,
+                    lng: 0.09185,
+                    alt: 37,
+                    name: 'CL',
+                    description: 'Cambridge William Gates Building (Computer Lab)'});
+
+/*
+2AE test gateway
+STATISTICS DEBUG
+    0-16-02-04-00-176
+Mantas test
+STATISTICS DEBUG
+    0-16-02-04-00-3C8
+University Library
+STATISTICS DEBUG
+    0-16-02-04-00-15C
+Sydney Sussex College
+STATISTICS DEBUG
+    0-16-02-04-00-14D
+Kelsey Kerridge
+STATISTICS DEBUG
+    0-16-02-04-00-14C
+Cambridge Consultants
+STATISTICS DEBUG
+    0-16-02-04-00-29D
+ARM
+STATISTICS DEBUG
+    0-16-02-04-00-1B4
+Microsoft
+STATISTICS DEBUG
+    0-16-02-04-00-2A5
+Shire Hall
+STATISTICS DEBUG
+    0-16-02-04-00-303
+Wilberforce Road Sports Ground
+STATISTICS DEBUG
+    0-16-02-04-00-249
+Phil Claridge
+STATISTICS DEBUG
+*/
